1b97f52cde44c2a15f2fc907cd10551c798b0f66
Liu Xinyu <40049653+ImCaviar@users.noreply.github.com>
2023-06-17 23:50:06 +0800

    Update Smali.java

--- ANDetect/src/main/java/iie/group5/structure/Smali.java
+++ ANDetect/src/main/java/iie/group5/structure/Smali.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-//Smali文件结构
+//Smali file structure
 public class Smali implements Serializable {
     //class name
     private String clazz;
