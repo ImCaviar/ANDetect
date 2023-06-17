f28fa03d8c75e8b07d4706fac18a4f73548c2f85
Liu Xinyu <40049653+ImCaviar@users.noreply.github.com>
2023-06-17 23:34:37 +0800

    Update Graph.java

--- ANDetect/src/main/java/iie/group5/structure/Graph.java
+++ ANDetect/src/main/java/iie/group5/structure/Graph.java
@@ -3,29 +3,32 @@ package iie.group5.structure;
 import java.util.*;
 
 public class Graph {
-    //图中原始节点，不变
+    // The original node in the graph, unchanged
     private Map<String, Vertex> vertices;
     private List<Vertex> vIDs;
-    //社区网络顶点计数器，节点ID，也是节点在vertices列表中的索引
+    // The community network vertex counter, node ID, is also the index of the node in the vertices list
     private int vtCount;
-    //记录社区C中所有节点、指向社区C中所有节点的权和，在多层Louvain中动态更新
+    // Record all nodes in community C, the sum of weights pointing to all nodes in community C, and the sum of weights pointing to all nodes in community C. Dynamically update in multi-layer Louvain
     private Map<Integer, List<Integer>> commV;
     private Map<Integer, Integer> commW;
+    private Map<Integer, Integer> commO;
 
     public Graph() {
         this.vertices = new HashMap<>();
         this.vIDs = new ArrayList<>();
         this.commV = new HashMap<>();
         this.commW = new HashMap<>();
+        //add
+        this.commO = new HashMap<>();
         this.vtCount = 0;
     }
 
-    //社区网络中的节点
+    // Nodes in the community network
     public class Vertex{
         private Integer ID;
         private String label;
         private Integer communityID;
-        //outEdges是V的出边，inEdges是V的入边，节点ID<->权重
+        // outEdges are the outgoing edges of V, inEdges are the incoming edges of V, node ID <-> weight
         private Map<Integer, Integer> outEdges;
         private Map<Integer, Integer> inEdges;
 
@@ -36,9 +39,9 @@ public class Graph {
             this.inEdges = new HashMap<>();
         }
 
-        //为顶点添加出边
+        // Adding out edges to vertices
         public void addOutEdge(int weight, Integer toID){
-            //如果已经存在该边，则叠加权重；如果不存在，则创建该边
+            // If the edge already exists, the weights are superimposed; if not, the edge is created
             if (this.outEdges.size() == 0 || !this.outEdges.containsKey(toID)){
                 this.outEdges.put(toID, weight);
             }else{
@@ -47,9 +50,9 @@ public class Graph {
             }
         }
 
-        //为顶点添加入边
+        // Adding an incoming edge to a vertex
         public void addInEdge(int weight, Integer inID){
-            //如果已经存在该边，则叠加权重；如果不存在，则创建该边
+            // If the edge already exists, the weights are superimposed; if not, the edge is created
             if (this.inEdges.size() == 0 || !this.inEdges.containsKey(inID)){
                 this.inEdges.put(inID, weight);
             }else{
@@ -58,7 +61,7 @@ public class Graph {
             }
         }
 
-        //查询本节点的所有出度
+        // Query all out degrees of this node
         public int selectOutWeight(){
             int outWeight = 0;
             if (this.outEdges.size() > 0){
@@ -69,7 +72,7 @@ public class Graph {
             return outWeight;
         }
 
-        //查询本节点的所有入度
+        // Query all the incoming degrees of this node
         public int selectInWeight(){
             int inWeight = 0;
             if (this.inEdges.size() > 0){
@@ -105,9 +108,8 @@ public class Graph {
         }
     }
 
-    //由真实节点构建图结构，需要Label！！！
 
-    //在图中插入未出现过的节点，同步插入，VertexID=this.vertices.index=this.labelsV.index
+    // Insert nodes that have not appeared in the graph, synchronously, VertexID=this.vertices.index=this.labelsV.index
     public void insertVertex(String label){
         if (!this.vertices.containsKey(label)){
             Vertex vertex = new Vertex(this.vtCount++, label);
@@ -116,7 +118,7 @@ public class Graph {
         }
     }
 
-    //在图中插入边，前提是fromLabel和toLabel都在图中存在
+    // Insert an edge in the graph, provided that both fromLabel and toLabel are present in the graph
     public void insertEdge(String fromLabel, String toLabel, int weight){
         Vertex fromV = this.vertices.get(fromLabel);
         Vertex toV = this.vertices.get(toLabel);
@@ -128,23 +130,8 @@ public class Graph {
         }
     }
 
-//    //由超级节点构建图结构，不需要Label！！！也不需要记录this.vertices了
-//
-//    //插入节点时返回节点ID
-//    private Integer insertSuperV(){
-//        Vertex vertex = new Vertex(this.vtCount++, "");
-//        this.vIDs.add(vertex);
-//        return vertex.getID();
-//    }
-//
-//    private void insertSuperE(Integer fromID, Integer toID, int weight){
-//        Vertex fromV = this.vIDs.get(fromID);
-//        Vertex toV = this.vIDs.get(toID);
-//        fromV.addOutEdge(weight, toID);
-//        toV.addInEdge(weight, fromID);
-//    }
 
-    //计算图中所有边的权重之和
+    // Calculate the sum of the weights of all edges in the graph
     private int sumWeight(){
         int sw = 0;
         for (Vertex v : this.vIDs){
@@ -153,7 +140,7 @@ public class Graph {
         return sw;
     }
 
-    //给定fromID和toID，返回边权重
+    // Given fromID and toID, return the edge weights
     private int findWeight(Integer fromID, Integer toID){
         int result = 0;
         Vertex fromV = this.vIDs.get(fromID);
@@ -163,12 +150,12 @@ public class Graph {
         return result;
     }
 
-    //查询给定label是否为图中Vertex
+    // Query whether the given label is the Vertex in the graph
     public boolean inGraph(String label){
         return this.vertices.containsKey(label);
     }
 
-    //查询给定label对应的社区ID
+    // Query the community ID corresponding to the given label
     public Integer commIDbyLable(String label){
         Integer commID = -1;
         if (!inGraph(label)){
@@ -180,7 +167,7 @@ public class Graph {
         return commID;
     }
 
-    //查询给定label对应节点的社区贡献度
+    // Query the community contribution of the node corresponding to a given label
     public int commContribute(String label){
         int contri = 0;
         if (!inGraph(label)){
@@ -202,7 +189,7 @@ public class Graph {
         return contri;
     }
 
-    //test...delete 查询给定label对应的节点Vertex的输入节点label和输出节点label
+    //test...delete Query the input node label and output node label of the node Vertex corresponding to the given label
     public String[] getInOutLabels(String label){
         String[] result = new String[]{"",""};
         if (!inGraph(label)){
@@ -227,7 +214,7 @@ public class Graph {
         return result;
     }
 
-    //通过图中节点ID获取该节点的社区ID
+    // Get the community ID of the node by the node ID in the graph
     public Integer getCommbyV(Integer vID){
         Vertex vertex = this.vIDs.get(vID);
         return vertex.getCommunityID();
@@ -241,24 +228,26 @@ public class Graph {
         return this.vtCount;
     }
 
-    //Louvain社区划分，返回每个节点属于的社区
+    // Louvain, returning the community to which each node belongs
 
-    //计算将节点V划入社区comm的模块度增量deltaQ
     private double deltaQ(Integer VID, Integer commID, double w){
-        int k_i_in = weight_iC(VID, commID);
-        int sumTOT = this.commW.get(commID);
-        int k_i = this.vIDs.get(VID).selectInWeight();
-        return 2*k_i_in - sumTOT*k_i/w;
+        // Node VID Original Community
+        Vertex tmpV = this.vIDs.get(VID);
+        Integer origComm = tmpV.getCommunityID();
+        int deltaW = weight_Ci(commID, VID) + weight_iC(VID, commID) - weight_Ci(origComm, VID) - weight_Ci(VID, origComm);
+        int sumTOT = (tmpV.selectOutWeight() * this.commW.get(commID) + tmpV.selectInWeight() * this.commO.get(commID)) -
+                (tmpV.selectOutWeight() * (this.commW.get(origComm) - tmpV.selectInWeight()) + tmpV.selectInWeight() * (this.commO.get(origComm) - tmpV.selectOutWeight()));
+        return deltaW - sumTOT/w;
     }
 
-    //计算整个社区的模块度Q
+    // Calculate the modularity of the whole community Q
     private double calQ(double w){
         double Q = 0;
         for (Vertex i : this.vIDs){
             for (Vertex j : this.vIDs){
                 if (i != j && i.getCommunityID().equals(j.getCommunityID())){
                     double w_ij = findWeight(i.getID(), j.getID());
-                    int sisj = i.selectInWeight() * j.selectOutWeight();
+                    int sisj = i.selectOutWeight() * j.selectInWeight();
                     Q += w_ij - sisj/w;
                 }
             }
@@ -266,7 +255,7 @@ public class Graph {
         return Q / w;
     }
 
-    //初始化社区映射
+    // Initializing Community Mapping
     private void initC(){
         for (Vertex v : this.vIDs){
             int commID = v.getCommunityID();
@@ -276,17 +265,23 @@ public class Graph {
                 this.commV.put(commID, vs);
                 int weight = v.selectInWeight();
                 this.commW.put(commID, weight);
+                //add
+                weight = v.selectOutWeight();
+                this.commO.put(commID, weight);
             }else{
                 List<Integer> vs = this.commV.get(commID);
                 vs.add(v.getID());
                 this.commV.put(commID, vs);
                 int weight = v.selectInWeight();
                 this.commW.put(commID, weight + this.commW.get(commID));
+                //add
+                weight = v.selectOutWeight();
+                this.commO.put(commID, weight + this.commO.get(commID));
             }
         }
     }
 
-    //计算从节点i指向社区C的权和
+    // Compute the weight sum from node i to community C
     private int weight_iC(int vID, int commID){
         List<Integer> toV = this.commV.get(commID);
         int weight = 0;
@@ -296,21 +291,42 @@ public class Graph {
         return weight;
     }
 
-    //单层Louvain实现
+    // Compute the weight sum from community C to node i
+    private int weight_Ci(int commID, int vID){
+        List<Integer> fromV = this.commV.get(commID);
+        int weight = 0;
+        for (Integer j : fromV){
+            weight += findWeight(j, vID);
+        }
+        return weight;
+    }
+
+    //rollback Restore the node community
+    private void rollback(Map<Integer, Integer> origComm){
+        for (Integer i : origComm.keySet()){
+            Vertex v = this.vIDs.get(i);
+            v.setCommunityID(origComm.get(i));
+        }
+    }
+
+    //Single-Louvain
     public double singleLouvain(){
-        //初始化社区编号，每个节点的社区编号与其ID相等
+        // Initialize the community number, each node's community number is equal to its ID
         for (Vertex v : this.vIDs){
             v.setCommunityID(v.getID());
         }
-        //初始化社区映射
+        // Initializing Community Mapping
         double w = sumWeight();
         initC();
         double lastQ = calQ(w);
         boolean flag = true;
         while (flag){
             flag = false;
+            // Record the original community number
+            Map<Integer, Integer> id2comm = new HashMap<>();
             for (Vertex v : this.vIDs){
                 Integer vID = v.getID();
+                id2comm.put(vID, v.getCommunityID());
                 if (v.getOutEdges().size() > 0){
                     double maxQ = 0;
                     Integer maxNeigh = -1;
@@ -318,7 +334,7 @@ public class Graph {
                     for (Integer neigh : v.getOutEdges().keySet()){
                         Integer neighComm = this.vIDs.get(neigh).getCommunityID();
                         if (!oldCommID.equals(neighComm)){
-                            //尝试将节点v划入节点neigh所在社区
+                            // Try to assign node v to the community where node neigh is located
                             double deltaQ = deltaQ(vID, neighComm, w);
                             if (deltaQ > maxQ){
                                 maxQ = deltaQ;
@@ -326,21 +342,22 @@ public class Graph {
                             }
                         }
                     }
-                    //如果maxNeigh不为-1，则将当前节点划入maxNeigh所在社区
+                    // If maxNeigh is not -1, then the current node is assigned to the community where maxNeigh is located
                     if (maxNeigh != -1){
                         v.setCommunityID(maxNeigh);
-                        //更新this.commV
+                        // Update this.commV
                         List<Integer> oldComm = this.commV.get(oldCommID);
                         List<Integer> newComm = this.commV.get(maxNeigh);
                         oldComm.remove(v.getID());
                         newComm.add(v.getID());
                         this.commV.put(oldCommID, oldComm);
                         this.commV.put(maxNeigh, newComm);
-                        //更新this.commW
-                        int oldWeight = this.commW.get(oldCommID);
-                        int newWeight = this.commW.get(maxNeigh);
-                        this.commW.put(oldCommID, oldWeight-v.selectInWeight());
-                        this.commW.put(maxNeigh, newWeight+v.selectInWeight());
+                        // Update this.commW
+                        this.commW.put(oldCommID, this.commW.get(oldCommID)-v.selectInWeight());
+                        this.commW.put(maxNeigh, this.commW.get(maxNeigh)+v.selectInWeight());
+                        // Update this.commO
+                        this.commO.put(oldCommID, this.commO.get(oldCommID)-v.selectOutWeight());
+                        this.commO.put(maxNeigh, this.commO.get(maxNeigh)+v.selectOutWeight());
                     }
                     if (maxQ > 0){
                         flag = true;
@@ -350,6 +367,8 @@ public class Graph {
             double allQ = calQ(w);
             double deltaQ = allQ-lastQ;
             if (deltaQ<=0){
+                //add rollback
+                rollback(id2comm);
                 break;
             }else {
                 lastQ = allQ;
@@ -358,7 +377,7 @@ public class Graph {
         return lastQ;
     }
 
-    //统计现存的社区列表，以及每个社区的所有节点包名
+    // Count the list of existing communities and the names of all node packages for each community
     private Map<Integer, List<String>> countComm(){
         Map<Integer, List<String>> commLabel = new HashMap<>();
 
@@ -376,7 +395,7 @@ public class Graph {
         return commLabel;
     }
 
-    //输出一共有多少个社区以及每个社区的所有节点包名
+    // Output how many communities there are and the names of all node packages for each community
     public void outputComm(){
         Map<Integer, List<String>> commLabel = countComm();
         System.out.printf("Total %d communities!\n", commLabel.size());
@@ -388,52 +407,4 @@ public class Graph {
             System.out.println("\n--------------------");
         }
     }
-
-//    //融合当前图社区中的节点形成超级节点，插入边，生成新的图结构，原始图节点所在社区ID<->新融合社区ID
-//    private Map<Integer, Integer> mergeComm(Graph graph){
-//        Map<Integer, Integer> old2new = new HashMap<>();
-//        for (Integer oldVID : this.commV.keySet()){
-//            Integer newVID = graph.insertSuperV();
-//            old2new.put(oldVID, newVID);
-//        }
-//        //遍历当前图节点，插入边权重
-//        for (Vertex fromV : this.vIDs){
-//            for (Integer toID : fromV.getOutEdges().keySet()){
-//                Vertex toV = this.vIDs.get(toID);
-//                Integer fromC = old2new.get(fromV.getCommunityID());
-//                Integer toC = old2new.get(toV.getCommunityID());
-//                int weight = fromV.getOutEdges().get(toID);
-//                graph.insertSuperE(fromC, toC, weight);
-//            }
-//        }
-//        return old2new;
-//    }
-//
-//    //多层Louvain实现
-//    public void multiLouvain(){
-//        //基于真实节点实现单层Louvain
-//        double Q = singleLouvain();
-//        while (true){
-//            //构建新图，融合当前图中节点
-//            Graph graph = new Graph();
-//            //超级节点ID<->当前图中社区
-//            Map<Integer, Integer> old2new = mergeComm(graph);
-//            double nowQ = graph.singleLouvain();
-//            if (Q > nowQ){
-//                break;
-//            }else{
-//                //将超级节点的社区ID赋给当前图中节点
-//                for (Integer oldID : old2new.keySet()){
-//                    Integer newID = graph.getCommbyV(old2new.get(oldID));
-//                    List<Integer> vlist = this.commV.get(oldID);
-//                    for (Integer i : vlist){
-//                        this.vIDs.get(i).setCommunityID(newID);
-//                    }
-//                }
-//                Q = nowQ;
-//            }
-//        }
-//        //输出当前社区划分结果
-//        outputComm();
-//    }
 }
