1d4db062f8a78e4677111a01078ecb6a4e21cd96
Liu Xinyu <40049653+ImCaviar@users.noreply.github.com>
2023-06-17 23:35:30 +0800

    Update Smali.java

--- ANDetect/src/main/java/iie/group5/structure/Smali.java
+++ ANDetect/src/main/java/iie/group5/structure/Smali.java
@@ -8,28 +8,27 @@ import java.util.*;
 
 //Smali文件结构
 public class Smali implements Serializable {
-    //class类名
+    //class name
     private String clazz;
-    //super父类，继承关系
+    //super
     private transient String supper;
-    //method数组中的每一项都由访问权限、参数类型列表、返回类型拼接而成
+    // Each item in the method array is composed of access rights, a list of parameter types, and return types
     private String[] method;
-    //field数组中的每一项都由是否静态、访问权限、字段类型拼接而成
+    // Each item in the field array is stitched together by whether it is static, access rights, and field type
     private String[] field;
-    //smali文件中调用的函数，不区分系统函数和自定义函数
+    // Functions called in smali files, without distinguishing between system functions and custom functions
     private transient String[] invoke;
-    //smali文件中调用的方法来自的方法索引
+    // The method index from which the method called in the smali file is derived
     private transient int[] invokeFromMethod;
-    //smali文件中调用的方法是否来自系统函数 1：来自系统函数；0：自定义函数
+    // Whether the method called in the smali file is from a system function 1: from a system function; 0: a custom function
     private transient int[] invokeFromSys;
-    //smali文件所在叶子节点ID
+    // The leaf node ID where the smali file is located
     private Integer ppID;
 
-    //定义常量
     private final int maxlen = 20;
-    //访问权限，常量，其中什么也不写表示default，用0，1，2，3分别表示以下四个访问权限
+    // Access Permissions
     private final String[] access = new String[]{"public","protected","default","private"};
-    //类型描述符，常量
+    // Type descriptors
     private final String[] types = new String[]{"Z","B","S","C","I","J","F","D","V","["};
 
     public Smali() {
@@ -43,19 +42,17 @@ public class Smali implements Serializable {
         this.invokeFromMethod = new int[maxlen];
         this.invokeFromSys = new int[maxlen];
 
-        //初始化invokeFromMethod所有值为-1
         Arrays.fill(invokeFromMethod, -1);
-        //初始化invokeFromSys所有值为true
         Arrays.fill(invokeFromSys, 1);
     }
 
-    //根据smali文件所在路径分析该smali文件，分别生成method field sys_invoke pkg_invoke
+    // Analyze the smali file according to its path and generate method field sys_invoke pkg_invoke respectively
     public void analyzeSmali(String filePath){
         try {
             List<String> allLines = Files.readAllLines(Paths.get(filePath));
             int methodInd = 0;
             for (String line: allLines){
-                //以一个或多个空格分割字符串，删除空元素
+                // Split the string with one or more spaces and remove the empty elements
                 String[] elements = line.split("\\s+");
                 if (elements.length == 1 && elements[0].length() == 0){
                     continue;
@@ -93,7 +90,7 @@ public class Smali implements Serializable {
         }
     }
 
-    //处理class名称/super父类
+    // Handling class names/super
     private String analyzeCS(String[] elements){
         String result = elements[elements.length-1];
         result = result.replaceFirst("L","");
@@ -101,21 +98,18 @@ public class Smali implements Serializable {
         return result;
     }
 
-    //处理field字段，返回"字段访问权限 字段类型"
+    // Handles field fields, returning "field access rights field type"
     private String analyzeField(String[] elements){
         String result = "";
-        //.field后为字段访问权限，如果为空则是"default"
         int inda = getIndex(this.access, elements[1]);
         if (inda == -1){
             result += "2 ";
         }else{
             result += inda + " ";
         }
-        //静态字段则加上"s"，小写！！！区分于短整数型
         if (getIndex(elements, "static") != -1){
             result += "s ";
         }
-        //查找字段类型并处理，通常为:后的字符串
         for (int i=1; i<elements.length; i++){
             int ind = elements[i].indexOf(":");
             if (ind != -1){
@@ -133,21 +127,18 @@ public class Smali implements Serializable {
         return result;
     }
 
-    //处理method字段，返回"类访问权限 参数类型列表 返回类型"
+    // Processes the method field, returning "Class access rights List of parameter types Return type"
     private String analyzeMethod(String[] elements){
         String result = "";
-        //.class后为类访问权限，如果为空则是"default"
         int inda = getIndex(this.access, elements[1]);
         if (inda == -1){
             result += "2 ";
         }else{
             result += inda + " ";
         }
-        //静态字段则加上"static"
         if (getIndex(elements, "static") != -1){
             result += "s ";
         }
-        //查找括号内的所有参数类型用_分隔，首先用;分割，然后判断是否存在多字符、“L”前是否有types中字母
         for (int i=1; i<elements.length; i++){
             int ind = elements[i].indexOf("(");
             if (ind != -1){
@@ -164,7 +155,6 @@ public class Smali implements Serializable {
                         result += getTypes(type);
                     }
                 }
-                //处理返回类型
                 if (ret.indexOf("L") == 0 || ret.indexOf("L") == 1){
                     ret = ret.replaceFirst("L", "");
                 }
@@ -178,36 +168,36 @@ public class Smali implements Serializable {
         return result;
     }
 
-    //处理method中的invoke函数调用，返回"调用函数包名->函数名"
+    // Handles invoke function calls in method, returning "calling function package name -> function name"
     private String analyzeInvoke(String[] elements){
         String[] tmp = elements[elements.length-1].split("->");
         String pkg = tmp[0];
         String func = tmp[1];
-        //处理调用函数包名
+
         int ind0 = pkg.indexOf('L');
         int ind3 = pkg.indexOf(";");
         if (ind0 == -1 && ind3 == -1){
             return "";
         }
         pkg = pkg.substring(ind0+1, ind3);
-        //处理调用函数名
+
         int ind1 = func.indexOf(">");
         int ind2 = func.indexOf("(");
         func = func.substring(ind1+1, ind2);
         return pkg + "->" + func;
     }
 
-    //分割字符串中的type
+    // Splitting the type in a string
     private String getTypes(String type) {
         String result = "";
         if (type.contains("/")) {
             int ind = type.indexOf("L");
             if (ind == 0) {
-                //"L"在第一位，删除"L"
+
                 result = type.replaceFirst("L", "") + " ";
                 return result;
             } else if (ind > 0 && ind < type.indexOf("/")) {
-                //"L"不在第一位，但在"/"前，说明"L"前还有别的类型
+
                 for (int i = 0; i < ind; i++) {
                     String sub = type.substring(i, i + 1);
                     if (getIndex(this.types, sub) != -1) {
@@ -217,15 +207,12 @@ public class Smali implements Serializable {
                 result += type.substring(ind+1) + " ";
                 return result;
             }else{
-                //没有"L"，直接返回
                 return type + " ";
             }
         }else{
-            //"L"在第一位，删除"L"
             if (type.contains("L") && type.indexOf("L") == 0){
                 type = type.substring(1);
             }
-            //剩余类中包含$表明是自定义类
             if (type.contains("$")){
                 int ind = type.indexOf("$");
                 result += type.substring(ind);
@@ -241,7 +228,7 @@ public class Smali implements Serializable {
         }
     }
 
-    //查找某个元素是否在String列表中，在则返回索引，不在则返回-1
+    // Find if an element is in the String list, return the index if it is, or -1 if it is not.
     private int getIndex(String[] list, String ele){
         int result = -1;
         if (list.length == 0){
@@ -256,7 +243,7 @@ public class Smali implements Serializable {
         }
     }
 
-    //在String数组中添加字符串
+    // Add a string to the String array
     private String[] addString(String[] list, String ele){
         int nowlen = list.length;
         if (nowlen > maxlen){
@@ -272,7 +259,7 @@ public class Smali implements Serializable {
         }
     }
 
-    //开辟新String数组
+    // Open a new String array
     private String[] newString(String[] oldList, String newele){
         int oldlen = oldList.length;
         String[] newList = new String[oldlen+1];
@@ -283,7 +270,7 @@ public class Smali implements Serializable {
         return newList;
     }
 
-    //在int数组中添加数字
+    // Adding numbers to an int array
     private int[] addInt(int[] list, int value){
         int nowlen = list.length;
         if (nowlen > maxlen){
@@ -299,7 +286,7 @@ public class Smali implements Serializable {
         }
     }
 
-    //开辟int新数组
+    // New int arrays
     private int[] newInt(int[] oldList, int newele){
         int oldlen = oldList.length;
         int[] newList = new int[oldlen+1];
@@ -310,9 +297,8 @@ public class Smali implements Serializable {
         return newList;
     }
 
-    //删除String数组中的指定元素
+    // Delete the specified element from the String array
     private String[] deleteNull(String[] strings){
-        //定义一个List列表，循环赋值
         ArrayList<String> strList = new ArrayList<>();
         for (int i=0; i<strings.length; i++){
             if (strings[i].length() > 0){
@@ -322,7 +308,7 @@ public class Smali implements Serializable {
         return strList.toArray(new String[strList.size()]);
     }
 
-    //对invoke和其所属methodID去重
+    // De-duplicate the invoke and the methodID it belongs to
     public void deleteRepInv(){
         if (this.invoke.length > 1){
             Map<Integer, List<String>> id2inv = new HashMap<>();
@@ -353,12 +339,11 @@ public class Smali implements Serializable {
         }
     }
 
-    //将invoke函数设置为自定义函数，修改invokeFromSys对应位置的值为0
+    // Set the invoke function to a custom function and change the value of the corresponding position of invokeFromSys to 0
     public void setInvoketoCtm(int index){
         this.invokeFromSys[index] = 0;
     }
 
-    //设置、获取clazz supper method field sys_invoke pkg_invoke ppID
     public String getClazz() {
         return clazz;
     }
