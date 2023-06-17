a548414177e30b7b047f32bcec4ab05d4694a8cf
Liu Xinyu <40049653+ImCaviar@users.noreply.github.com>
2023-06-17 23:26:36 +0800

    Update MatchbyRes.java

--- ANDetect/src/main/java/iie/group5/APKprocess/MatchbyRes.java
+++ ANDetect/src/main/java/iie/group5/APKprocess/MatchbyRes.java
@@ -1,6 +1,5 @@
 package iie.group5.APKprocess;
 
-import com.android.tools.r8.u.a.a.a.h.S;
 import com.opencsv.exceptions.CsvValidationException;
 import iie.group5.features.StringAndAPI;
 
@@ -14,23 +13,26 @@ public class MatchbyRes {
     private float maxMatched;
     private String matchAN;
     private List<String> AdKeys;
+    private double maxTI;
 
-    public MatchbyRes(ResProcess apkRes, Map<String, ResProcess> anRes, Map<String, Map<String, Double>> anWeights) throws IOException, CsvValidationException {
+    public MatchbyRes(ResProcess apkRes, Map<String, ResProcess> anRes, Map<String, Map<String, Double>> anWeights, double maxTI) throws IOException, CsvValidationException {
         this.apkRes = apkRes;
         this.anRes = anRes;
         this.anWeights = anWeights;
         this.maxMatched = 0;
         StringAndAPI saa = new StringAndAPI();
         this.AdKeys = saa.getStrings();
+        this.maxTI = maxTI;
     }
 
-    public Map<String, Double> compMaxMatch(double thresh){
+    // thresh is the threshold, pc is the value of omega2/omega1
+    public Map<String, Double> compMaxMatch(double thresh, double pc){
         Map<String, Double> result = new HashMap<>();
         for (String anName : this.anRes.keySet()){
-            double sim = compSim(this.anRes.get(anName), this.apkRes);
+            double sim = compSim(this.anRes.get(anName), this.apkRes, pc);
             //template
             if (sim >= thresh){
-                //判断是否有前缀相似的，取sim值最大者
+                // Determine if there are similar prefixes, and take the one with the largest sim value
                 String cmp = matchPrefix(result, anName);
                 if (cmp == null){
                     result.put(anName,sim);
@@ -46,20 +48,20 @@ public class MatchbyRes {
         return result;
     }
 
-    private double compSim(ResProcess an, ResProcess apk){
+    private double compSim(ResProcess an, ResProcess apk, double pc){
         double f1_sim = fullDomain(an.getAll_an(), apk.getAll_an());
         double final_sim = f1_sim;
         if (an.getPermission().size()>0){
             double f2_sim = fullMatch(an.getPermission(), apk.getPermission());
-            final_sim += 0.5*f2_sim;
+            final_sim += pc*f2_sim;
         }
         if (an.getAuth().size()>0){
             double f3_sim = fullMatch(an.getAuth(), apk.getAuth());
-            final_sim += 0.5*f3_sim;
+            final_sim += pc*f3_sim;
         }
         if (an.getIntentFilter().size()>0){
             double f4_sim = fullMatch(an.getIntentFilter(), apk.getIntentFilter());
-            final_sim += f4_sim;
+            final_sim += pc*f4_sim;
         }
         if (an.getActivity().size()>0){
             double f5_sim = weightEdit(an.getActivity(), apk.getActivity());
@@ -75,34 +77,34 @@ public class MatchbyRes {
         }
         if (an.getDimen().size()>0){
             double f8_sim = ignorePath(an.getHostPKG(), an.getDimen(), apk.getDimen());
-            final_sim += 0.5*f8_sim;
+            final_sim += pc*f8_sim;
         }
         if (an.getStringn().size()>0){
             double f9_sim = ignorePath(an.getHostPKG(), an.getStringn(), apk.getStringn());
-            final_sim += 0.5*f9_sim;
+            final_sim += pc*f9_sim;
         }
         if (an.getAttr().size()>0){
             double f10_sim = ignorePath(an.getHostPKG(), an.getAttr(), apk.getAttr());
-            final_sim += 0.5*f10_sim;
+            final_sim += pc*f10_sim;
         }
         if (an.getStyle().size()>0){
             double f11_sim = ignorePath(an.getHostPKG(), an.getStyle(), apk.getStyle());
-            final_sim += 0.5*f11_sim;
+            final_sim += pc*f11_sim;
         }
         if (an.getPicNames().size()>0){
             double pic_sim = fullMatch(an.getPicNames(), apk.getPicNames());
-            final_sim += 0.5*pic_sim;
+            final_sim += pc*pic_sim;
         }
-        return final_sim;
+        return final_sim/(4+8*pc);
     }
 
-    //全域匹配
+    // Global Matching
     private double fullDomain(HashSet<String> anFD, HashSet<String> apkFD){
         double result = 0;
         for (String a : apkFD){
             for (String an : anFD){
                 if (a.contains(an)){
-                    result = 2;
+                    result = 1;
                     break;
                 }
             }
@@ -110,7 +112,7 @@ public class MatchbyRes {
         return result;
     }
 
-    //无路径匹配，要求anIP.size()>0
+    // No path matching, requires anIP.size()>0
     private double ignorePath(String anName, HashSet<String> anIP, HashSet<String> apkIP){
         double sumMatched = 0;
         int count = anIP.size();
@@ -118,7 +120,8 @@ public class MatchbyRes {
             for (String apk : apkIP){
                 if (an.equals(apk)){
                     Map<String, Double> tfidfs = this.anWeights.get(anName);
-                    Double weight = tfidfs.get(an);
+                    //归一化
+                    Double weight = tfidfs.get(an)/this.maxTI;
                     sumMatched += weight;
                     break;
                 }
@@ -127,7 +130,7 @@ public class MatchbyRes {
         return sumMatched/count;
     }
 
-    //完全路径匹配-固定特征、资源池匹配，要求anFM.size()>0
+    // Full path matching-stable feature, resource pool matching, requires anFM.size()>0
     private double fullMatch(HashSet<String> anFM, HashSet<String> apkFM){
         double result = 0;
         if (apkFM.size() > 0){
@@ -142,7 +145,7 @@ public class MatchbyRes {
         return result;
     }
 
-    //完全路径匹配-可变特征，加权编辑距离，找到最大匹配度均值，要求anWE.size()>0
+    // Full path matching-variable features, weighted edit distance, find maximum match mean, require anWE.size()>0
     private double weightEdit(HashSet<String> anWE, HashSet<String> apkWE){
         double sumMatch = 0;
         int count = anWE.size();
@@ -168,30 +171,30 @@ public class MatchbyRes {
         return sumMatch/count;
     }
 
-    //自定义激活函数
+    // Customized activation functions
     private double exp(int match_count){
         double result = 0;
         if (match_count > 0){
-            result = 2/(1+Math.exp(1-match_count));
+            result = 1/(1+Math.exp(1-match_count));
         }
         return result;
     }
 
-    //根据两个前缀相同字符串的加权编辑距离，计算字符串相似度
+    // Calculate the string similarity based on the weighted edit distance of two strings with the same prefix
     private double WE2Sim(String[] subAN, String[] subAPK){
         double result = 0;
-        //前缀不等则返回0
+        // Return 0 if prefix is not equal
         for (int i=0; i<3; i++){
             if (!subAN[i].equals(subAPK[i])){
                 return result;
             }
         }
-        //前缀相等但有一个字符串无后缀
+        // Equal prefix but one string without suffix
         if (subAN.length == 3 || subAPK.length == 3){
             result = 1;
             return result;
         }
-        //前缀相等，动态规划计算后缀的加权编辑距离
+        // Prefixes are equal, and dynamic planning calculates the weighted edit distance of the suffixes
         int d;
         int anSufLen = subAN.length-3;
         int apkSufLen = subAPK.length-3;
@@ -213,11 +216,11 @@ public class MatchbyRes {
                 M[i][j] = min(M[i][j-1]+rho(subAPK[3+j-1])*weight(apkSufLen,j-1), M[i-1][j]+weight(apkSufLen,j), M[i-1][j-1]+d*rho(subAPK[3+j-1])*weight(apkSufLen,j-1));
             }
         }
-        result = 2 - M[anSufLen][apkSufLen];
+        result = (2 - M[anSufLen][apkSufLen])/2;
         return result;
     }
 
-    //计算三者中最小值
+    // Calculate the smallest of the three
     private double min(double a, double b, double c){
         double result;
         if (a < b){
@@ -231,7 +234,7 @@ public class MatchbyRes {
         return result;
     }
 
-    //计算系数rho
+    // Calculation factor rho
     private int rho(String subStr){
         int result = 1;
         for (int i=0; i<AdKeys.size(); i++){
@@ -243,7 +246,7 @@ public class MatchbyRes {
         return result;
     }
 
-    //计算权重，len为后缀长度，ind为字符串节索引，0 to len-1
+    // Calculate the weight, len is the suffix length, ind is the string section index, 0 to len-1
     private double weight(int len, int ind){
         double deno = 0;
         for (int i=0; i<len; i++){
@@ -253,7 +256,7 @@ public class MatchbyRes {
         return mole/deno;
     }
 
-    //返回map中前缀匹配>=3的字符串
+    // Returns the string in map with prefix match >= 3
     private String matchPrefix(Map<String, Double> map, String str){
         String ret = null;
         if (str.contains(".")){
